e8b8264fe12338134743fc16a19bf21d09c8821a
xhsmikeli-create <xhsmikeli@gmail.com>
2026-04-14 19:12:04 +0800

    第四次提交

--- executor/tests/executor_test.rs
+++ executor/tests/executor_test.rs
@@ -119,3 +119,63 @@ mod turn_left_tests {
         assert_eq!(expected_pose, executor.query());
     }
 }
+// 验证 R 指令在不同朝向下的行为。
+mod turn_right_tests {
+    use super::*;
+
+    #[test]
+    fn should_return_heading_s_given_command_is_r_and_facing_is_e() {
+        // 初始位置在原点，朝东方向。
+        let original_pose = Pose::new(0, 0, 'E');
+        let mut executor = Executor::new(original_pose);
+
+        // 执行一次右转指令。
+        executor.execute("R");
+
+        // 按照需求文档，车辆位置不变，方向应调整为朝南。
+        let expected_pose = Pose::new(0, 0, 'S');
+        assert_eq!(expected_pose, executor.query());
+    }
+
+    #[test]
+    fn should_return_heading_w_given_command_is_r_and_facing_is_s() {
+        // 初始位置在原点，朝南方向。
+        let original_pose = Pose::new(0, 0, 'S');
+        let mut executor = Executor::new(original_pose);
+
+        // 执行一次右转指令。
+        executor.execute("R");
+
+        // 按照需求文档，车辆位置不变，方向应调整为朝西。
+        let expected_pose = Pose::new(0, 0, 'W');
+        assert_eq!(expected_pose, executor.query());
+    }
+
+    #[test]
+    fn should_return_heading_n_given_command_is_r_and_facing_is_w() {
+        // 初始位置在原点，朝西方向。
+        let original_pose = Pose::new(0, 0, 'W');
+        let mut executor = Executor::new(original_pose);
+
+        // 执行一次右转指令。
+        executor.execute("R");
+
+        // 按照需求文档，车辆位置不变，方向应调整为朝北。
+        let expected_pose = Pose::new(0, 0, 'N');
+        assert_eq!(expected_pose, executor.query());
+    }
+
+    #[test]
+    fn should_return_heading_e_given_command_is_r_and_facing_is_n() {
+        // 初始位置在原点，朝北方向。
+        let original_pose = Pose::new(0, 0, 'N');
+        let mut executor = Executor::new(original_pose);
+
+        // 执行一次右转指令。
+        executor.execute("R");
+
+        // 按照需求文档，车辆位置不变，方向应调整为朝东。
+        let expected_pose = Pose::new(0, 0, 'E');
+        assert_eq!(expected_pose, executor.query());
+    }
+}
